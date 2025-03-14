22e703193c94d3be3dd048c50e75f05bfc4e574d
Renato Golia <renato@golia.tech>
2025-03-14 12:21:22 +0100

    Project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# ResXporter
+
+ResXporter is a simple tool to export and import resx files. It is a command line tool that can be used in a build pipeline to export resx files from a project and then import again after translations were done.
---
+++ src/ResXporter/Program.cs
@@ -0,0 +1,10 @@
+﻿using Spectre.Console.Cli;
+using Microsoft.Extensions.DependencyInjection;
+
+using Spectre.Console.Cli.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+var registrar = new DependencyInjectionRegistrar(services);
+
+var app = new CommandApp(registrar);
