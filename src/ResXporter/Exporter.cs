79dcd0e327333db2adaca427eee9278765fc2b53
Renato Golia <renato@golia.tech>
2025-03-17 23:32:38 +0100

    Export to Microsoft Lists (#1)
    * Added Microsoft Lists exporter
    
    * Refined exporter

--- src/ResXporter/Exporter.cs
+++ src/ResXporter/Exporter.cs
@@ -2,5 +2,6 @@
 
 public enum Exporter
 {
-    JetBrainsCsv
+    JetBrainsCsv,
+    MicrosoftLists
 }
--- src/ResXporter/Program.cs
+++ src/ResXporter/Program.cs
@@ -9,8 +9,10 @@ using Spectre.Console.Cli.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 
+services.AddHttpClient();
 services.AddSingleton(TimeProvider.System);
 services.AddKeyedScoped<IExporter, JetBrainsCsvExporter>(Exporter.JetBrainsCsv);
+services.AddKeyedScoped<IExporter, MicrosoftListsExporter>(Exporter.MicrosoftLists);
 
 var registrar = new DependencyInjectionRegistrar(services);
 
